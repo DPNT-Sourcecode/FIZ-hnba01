749f3bcd2d0525239482394c20ac08c2ae21d1b0
sbx_user1060 <sbx_user1060@ip-10-129-55-244.eu-west-2.compute.internal>
2018-09-12 10:29:18 +0000

    Wed Sep 12 10:29:18 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,24 +5,22 @@ import static java.lang.Integer.valueOf;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if (isFizz(number) && isBuzz(number) && isDeluxe(number)) {
-            return "fizz buzz deluxe";
+        final Optional<String> deluxWord = getDelux(number);
+        if (isFizz(number) && isBuzz(number) && deluxWord.isPresent()) {
+            return "fizz buzz " + deluxWord.get();
         } else if (isFizz(number) && isBuzz(number)) {
             return "fizz buzz";
         } else if (isFizz(number)) {
-            return (isDeluxe(number)) ? "fizz deluxe" : "fizz";
+            return deluxWord.map(s -> "fizz " + s).orElse("fizz");
         } else if (isBuzz(number)) {
-            return (isDeluxe(number)) ? "buzz deluxe" : "buzz";
-        } else if (isDeluxe(number)) {
-            return "deluxe";
-        } else {
-            return number.toString();
-        }
+            return deluxWord.map(s -> "buzz " + s).orElse("buzz");
+        } else return deluxWord.orElseGet(number::toString);
 
     }
 
@@ -39,14 +37,20 @@ public class FizzBuzzSolution {
         return number % expectedNumber == 0 || number.toString().contains(expectedNumber.toString());
     }
 
-    private boolean isDeluxe(Integer number) {
+    private Optional<String> getDelux(Integer number) {
 
         // no need to process stream if number is less than 10
         if (number < 10) {
-            return false;
+            return Optional.empty();
         }
 
         final Map<String, List<String>> collect = Arrays.stream(number.toString().split("")).collect(Collectors.groupingBy(e -> e));
-        return collect.entrySet().size() == 1;
+        if (collect.entrySet().size() == 1 && number % 2 == 0) {
+            return Optional.of("fake deluxe");
+        } else if (collect.entrySet().size() == 1) {
+            return Optional.of("deluxe");
+        } else {
+            return Optional.empty();
+        }
     }
 }
